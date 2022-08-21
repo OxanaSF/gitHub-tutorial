e363fdd41dd4a0830e096d217b5954a38f968942
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-08-21 14:36:40 -0700

    feat(heading): Change Chapter 1

--- src/components/ChapterOne/ChapterOne.js
+++ src/components/ChapterOne/ChapterOne.js
@@ -3,7 +3,7 @@ import { ChapterOneStyled } from './ChapterOneStyles';
 const ChapterOne = () => {
   return (
     <ChapterOneStyled>
-      <h1>Chapter One</h1>
+      <h1>Setting up GitHub Account</h1>
     </ChapterOneStyled>
   );
 };
